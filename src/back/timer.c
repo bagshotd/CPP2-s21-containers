d0a9e4d962af66eac003d5fd37157a23d0a8a9d7
bagshot <119291066+bagshotd@users.noreply.github.com>
2025-02-12 00:13:20 +0300

    Delete src directory

--- src/back/timer.c
+++
@@ -1,9 +0,0 @@
-#include "backend.h"
-
-void sleepMilli(int milliseconds) {
-  struct timespec timer = {0};
-  timer.tv_sec = 0;
-  timer.tv_nsec = milliseconds * 1000 * 1000;
-
-  nanosleep(&timer, NULL);
-}
